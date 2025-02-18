0b07595ad5d4e08b95f9bd9d9b48902bb55a4b80
Themba <mndebele.themba@gmail.com>
2025-02-18 23:02:29 +0200

    Remove unused BrowserIcon component from AddBookmark; streamline codebase

--- components/bookmarks/add-bookmark.tsx
+++ components/bookmarks/add-bookmark.tsx
@@ -21,44 +21,6 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { Spinner } from "../ui/spinner";
 
-const BrowserIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={24}
-    height={24}
-    color={"#fafafa"}
-    fill={"none"}
-    {...props}
-  >
-    <path
-      d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
-      stroke="currentColor"
-      strokeWidth="1.5"
-    />
-    <path
-      d="M2.5 9H21.5"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M6.99981 6H7.00879"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M10.9998 6H11.0088"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
 const GalleryBoldIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     width="24"
