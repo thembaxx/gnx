d311fe03d930c1ab9a206ba54849d9112d2604ab
Themba <mndebele.themba@gmail.com>
2025-02-21 17:02:41 +0200

    Remove unused JobProps import from AddJobForm component

--- components/admin/job/add-job-form.tsx
+++ components/admin/job/add-job-form.tsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
-import { JobProps } from "@/types";
+// import { JobProps } from "@/types";
 import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
 import { Switch } from "@/components/ui/switch";
