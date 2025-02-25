014755d09e61f668247bcc68807fd26507502839
Themba <mndebele.themba@gmail.com>
2025-02-25 19:37:50 +0200

    up

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   // Warning: This allows production builds to successfully complete even if
   // your project has ESLint errors.
+  // !! WARN !!
   eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
@@ -18,6 +19,30 @@ const nextConfig: NextConfig = {
         pathname: "**",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/app/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://themba.dev",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
